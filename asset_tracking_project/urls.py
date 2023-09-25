65162483d6e423b3ac1a98e537eab7d1b8ead11e
polin103109 <sabrinpolinece18@gmail.com>
2023-09-25 20:28:50 +0600

    added endpoint for Api Documentation

--- asset_tracking_project/urls.py
+++ asset_tracking_project/urls.py
@@ -1,9 +1,11 @@
+from rest_framework.schemas import get_schema_view
 
 from django.contrib import admin
 from django.urls import path, include
-
+schema_view = get_schema_view(title='Corporate Asset Management API', public=True)
 
 urlpatterns = [
+    path('api/docs/', schema_view),
     path('admin/', admin.site.urls),
     path('api/', include('asset_tracking.urls')),
 ]
