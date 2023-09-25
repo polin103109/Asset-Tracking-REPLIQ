b09da31abf16c4f86dd9eb960beb0eecb9e6fc0c
polin103109 <sabrinpolinece18@gmail.com>
2023-09-25 20:16:32 +0600

    install DRF for api documentation

--- asset_tracking_project/settings.py
+++ asset_tracking_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'drf_yasg',
     'rest_framework',
     'asset_tracking',
     'django.contrib.admin',
@@ -40,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+REST_FRAMEWORK = {
+    # ...
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
