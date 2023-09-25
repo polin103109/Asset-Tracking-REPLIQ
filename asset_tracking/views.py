3a978694fcdded1c4482a01951c51c178ec44eb4
polin103109 <sabrinpolinece18@gmail.com>
2023-09-25 15:54:51 +0600

    added views for models and serializers

--- asset_tracking/views.py
+++ asset_tracking/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from .models import *
+from .serializers import *
+
+class CompanyListCreateView(generics.ListCreateAPIView):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+
+class EmployeeListCreateView(generics.ListCreateAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+class DeviceListCreateView(generics.ListCreateAPIView):
+    queryset = Device.objects.all()
+    serializer_class = DeviceSerializer
+
+class DeviceLogListCreateView(generics.ListCreateAPIView):
+    queryset = DeviceLog.objects.all()
+    serializer_class = DeviceLogSerializer
